e6c41be3a190d54f80c1a8fcd1dec50369dcd0c5
Ben Siraphob <bensiraphob@gmail.com>
2021-12-02 23:33:34 -0600

    Cleanup and add benchmarking code

--- day3.hs
+++ day3.hs
@@ -1,10 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 import Control.Applicative
@@ -44,9 +40,23 @@ toBinNum = foldl' f 0
     f n '1' = n * 2 + 1
     f n '0' = n * 2
 
-part1, part2 :: _ -> Int
-part1 i = undefined
-part2 i = undefined
+part1 inp' = (toBinNum x * toBinNum y)
+  where
+    f s = (mcb s, lcb s)
+    (x, y) = unzip $ map f inp'
+
+part2 inp = toBinNum (snd (head (f1 y))) * toBinNum (snd (head (f2 y)))
+  where
+    g f [x] = [x]
+    g f l = zip (tail <$> bs') ns'
+      where
+        bs = head . fst <$> l
+        (bs', ns') = unzip (filter ((== b) . head . fst) l)
+        b = f bs
+
+    f1 = fixedPoint (g mcb)
+    f2 = fixedPoint (g lcb)
+    y = map dup inp
 
 main = do
   let dayNumber = 3 :: Int
@@ -54,28 +64,12 @@ main = do
   let dayFilename = dayString <> ".txt"
   inp <- lines <$> readFile dayFilename
   let inp' = transpose inp
-  -- print (take 10 inp')
-  let f s = (mcb s, lcb s)
-  let (x, y) = unzip $ map f inp'
-  print (toBinNum x * toBinNum y)
-  let g f [x] = [x]
-      g f l = zip (tail <$> bs') ns'
-        where
-          bs = head . fst <$> l
-          (bs', ns') = unzip (filter ((== b) . head . fst) l)
-          b = f bs
-
-  let f1 = fixedPoint (g mcb)
-      f2 = fixedPoint (g lcb)
-      y = map dup inp
-  print (toBinNum (snd (head (f1 y))) * toBinNum (snd (head (f2 y))))
-
--- print (part1 inp)
--- print (part2 inp)
--- defaultMain
---   [ bgroup
---       dayString
---       [ bench "part1" $ whnf part1 inp,
---         bench "part2" $ whnf part2 inp
---       ]
---   ]
+  print (part1 inp')
+  print (part2 inp)
+  defaultMain
+    [ bgroup
+        dayString
+        [ bench "part1" $ whnf part1 inp,
+          bench "part2" $ whnf part2 inp
+        ]
+    ]
