30919c50e567eb0669435dd29e3161f87fc31a9e
Ben Siraphob <bensiraphob@gmail.com>
2021-12-17 13:48:03 -0600

    day15: switch to IntMap and IntSet
    Runtime down from 11s to 4s

--- day15.hs
+++ day15.hs
@@ -1,14 +1,26 @@
 {-# LANGUAGE TupleSections #-}
 
 import Data.Foldable
+import Data.Function
+import qualified Data.IntMap as IM
+import Data.IntMap.Strict (IntMap)
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IS
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Function
+
+type Grid = IntMap Int
+
+type Pt = Int
+
+conv :: (Int, Int) -> Pt
+conv (x, y) = 1000 * x + y
+
+from :: Pt -> (Int, Int)
+from n = (n `div` 1000, n `mod` 1000)
 
 toGrid :: [[Int]] -> Grid
-toGrid = M.fromList . concat . zipWith (\x l -> zipWith (curry (\((x, a), y) -> ((x, y), a))) (map (x,) l) [1 ..]) [1 ..]
+toGrid = IM.fromList . concat . zipWith (\x l -> zipWith (curry (\((x, a), y) -> (conv (x, y), a))) (map (x,) l) [1 ..]) [1 ..]
 
 {-
  1  function Dijkstra(Graph, source):
@@ -35,39 +47,39 @@ toGrid = M.fromList . concat . zipWith (\x l -> zipWith (curry (\((x, a), y) -> 
 22      return dist[], prev[]
 -}
 
-type Grid = Map (Int, Int) Int
+neighbors :: Grid -> Int -> IntSet
+neighbors g p = IS.filter (`IM.member` g) (IS.fromList (conv <$> [(x -1, y), (x, y + 1), (x + 1, y), (x, y -1)]))
+  where
+    (x, y) = from p
 
-neighbors :: Grid -> (Int, Int) -> Set (Int, Int)
-neighbors g (x, y) = S.filter (`M.member` g) (S.fromList [(x -1, y), (x, y + 1), (x + 1, y), (x, y -1)])
-
-dijkstra :: Map (Int, Int) Int -> (Int, Int) -> (Map (Int, Int) Int, Map (Int, Int) (Int, Int))
+dijkstra :: IntMap Int -> Int -> (IntMap Int, IntMap Int)
 dijkstra g s = step1 & step2 & step3
   where
-    step1 = let c = M.keysSet g; (a, b) = foldl' go (M.empty, M.empty) c in (a, b, c)
+    step1 = let c = IM.keysSet g; (a, b) = IS.foldl' go (mempty, mempty) c in (a, b, c)
     go (dv, pv) v = (dv', pv')
       where
-        dv' = M.insert v (maxBound :: Int) dv
-        pv' = M.insert v (0, 0) pv
-    step2 (dv, pv, q) = (M.insert s 0 dv, pv, q)
+        dv' = IM.insert v (maxBound :: Int) dv
+        pv' = IM.insert v (conv (0, 0)) pv
+    step2 (dv, pv, q) = (IM.insert s (conv (0, 0)) dv, pv, q)
     step3 (dv, pv, q)
-      | S.null q = (dv, pv)
+      | IS.null q = (dv, pv)
       | otherwise = step3 (dv', pv', q')
       where
-        q' = S.delete u q
+        q' = IS.delete u q
         u = nextVert dv
-        (dv', pv') = S.foldl' f (dv, pv) (S.intersection q (neighbors g u))
+        (dv', pv') = IS.foldl' f (dv, pv) (IS.intersection q (neighbors g u))
           where
             f (dv, pv) v = (dv'', pv'')
               where
-                alt = dv M.! u + g M.! v
-                (dv'', pv'') = if alt < dv M.! v then (M.insert v alt dv, M.insert v u pv) else (dv, pv)
-        nextVert :: Map (Int, Int) Int -> (Int, Int)
+                alt = dv IM.! u + g IM.! v
+                (dv'', pv'') = if alt < dv IM.! v then (IM.insert v alt dv, IM.insert v u pv) else (dv, pv)
+        nextVert :: IntMap Int -> Int
         nextVert dv = v
           where
-            v :: (Int, Int)
-            v = minimumBy (\x y -> compare (dv M.! x) (dv M.! y)) q
+            v :: Int
+            v = fst $ IS.foldl' (\(x, c) y -> if (dv IM.! y) < c then (y, dv IM.! y) else (x, c)) (undefined, maxBound :: Int) q
 
-part1 (inp, n) = fst (dijkstra inp (1, 1)) M.! (n, n)
+part1 (inp, n) = fst (dijkstra inp (conv (1, 1))) IM.! conv (n, n)
 
 main = do
   let dayNumber = 15 :: Int
@@ -78,11 +90,11 @@ main = do
   let n = length (head $ lines $ inp')
   print (part1 (inp, n))
 
--- print (part2 inp)
--- defaultMain
---   [ bgroup
---       dayString
---       [ bench "part1" $ whnf part1 inp,
---         bench "part2" $ whnf part2 inp
---       ]
---   ]
+-- -- print (part2 inp)
+-- -- defaultMain
+-- --   [ bgroup
+-- --       dayString
+-- --       [ bench "part1" $ whnf part1 inp,
+-- --         bench "part2" $ whnf part2 inp
+-- --       ]
+-- --   ]
